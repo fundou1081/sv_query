b586e93fb7ceb998ea372155a61bb05190aa6b80
fanghaobo <fanghaobo@gmail.com>
2026-05-17 00:30:34 +0800

    test: 添加 Issue 13/16 金标准测试
    - test_hierarchical_instance_support: 测试 HierarchicalInstance 节点类型支持
    - test_instance_no_duplicate: 测试实例不重复提取
    
    按照铁律13: 金标准测试要求添加

--- sim/tests/unit/test_instance_name_extraction.py
+++ sim/tests/unit/test_instance_name_extraction.py
@@ -106,5 +106,73 @@ endmodule'''
         self.assertEqual(type0, 'my_module')
 
 
+
+    def test_hierarchical_instance_support(self):
+        """Issue 13: 测试 HierarchicalInstance 节点类型支持
+        
+        bs_mult 等非ANSI模块使用 HierarchicalInstance 节点类型
+        """
+        source = '''
+module bs_mult();
+    bs_mult_slice I0(.clk(clk), .xy(xy));
+    bs_mult_slice I1(.clk(clk), .xy(xy));
+endmodule'''
+        
+        tree = pyslang.SyntaxTree.fromText(source)
+        
+        class FakeParser:
+            def __init__(self, tree):
+                self.trees = {'test': tree}
+        
+        parser = FakeParser(tree)
+        adapter = PyslangAdapter(parser)
+        instances = adapter.get_module_instances(parser.trees)
+        
+        # 应该提取到 2 个实例
+        self.assertEqual(len(instances), 2)
+        
+        # 验证实例名称
+        name0 = adapter.get_instance_name(instances[0])
+        type0 = adapter.get_instance_module_type(instances[0])
+        self.assertEqual(name0, 'I0')
+        self.assertEqual(type0, 'bs_mult_slice')
+        
+        name1 = adapter.get_instance_name(instances[1])
+        type1 = adapter.get_instance_module_type(instances[1])
+        self.assertEqual(name1, 'I1')
+        self.assertEqual(type1, 'bs_mult_slice')
+
+    def test_instance_no_duplicate(self):
+        """Issue 16: 测试实例不重复
+        
+        应该只有 HierarchyInstantiation 节点被提取
+        HierarchicalInstance 子节点不应该被重复提取
+        """
+        source = '''
+module test();
+    sub_module inst(.clk(clk), .d(d), .q(q));
+endmodule'''
+        
+        tree = pyslang.SyntaxTree.fromText(source)
+        
+        class FakeParser:
+            def __init__(self, tree):
+                self.trees = {'test': tree}
+        
+        parser = FakeParser(tree)
+        adapter = PyslangAdapter(parser)
+        instances = adapter.get_module_instances(parser.trees)
+        
+        # 应该只有 1 个实例，不是 2 个
+        self.assertEqual(len(instances), 1)
+        
+        # 实例名称应该是 inst，不是 None
+        name = adapter.get_instance_name(instances[0])
+        self.assertEqual(name, 'inst')
+        
+        # 类型应该是 sub_module
+        mtype = adapter.get_instance_module_type(instances[0])
+        self.assertEqual(mtype, 'sub_module')
+
 if __name__ == '__main__':
     unittest.main()
